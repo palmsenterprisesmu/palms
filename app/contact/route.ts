3f2e4090bdd4de12bcc8443d86d5887b5eb1af37
Stephanie Stein <stephzn@gmail.com>
2026-04-05 20:30:39 +0400

    Forms Fixes

--- app/contact/route.ts
+++ app/contact/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: NextRequest) {
     const { name, email, message } = await req.json()
 
     await resend.emails.send({
-      from: 'Palms Mauritius <hello@palmsmauritius.com>',
+      from: 'Palms Mauritius <onboarding@resend.dev>',
       to: process.env.YOUR_EMAIL!,
       subject: `💬 New message from ${name}`,
       html: `
